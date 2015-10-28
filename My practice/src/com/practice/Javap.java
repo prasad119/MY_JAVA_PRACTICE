072de8a610d4e9a8749f972c4b1e16c918dea570
prasad119 <119niki@gmail.com>
2015-10-28 14:44:47 +0530

    String Reverse

---
+++ My practice/src/com/practice/Javap.java
@@ -0,0 +1,26 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class Javap {
+	@Test(enabled=false)
+	public void f() {
+		String x = "prasad";
+		int i= x.length();
+		String r="";
+		for(int j=0;j<i;j++){
+			r= x.charAt(j)+r;
+		}
+		System.out.println(r);
+	}
+	@Test
+	public void d(){
+		String x = "prasad";
+		String y[] = x.split("");
+		String r="";
+		for(String z:y){
+		r=z+r;		
+		}
+		System.out.println(r);
+	}
+}
